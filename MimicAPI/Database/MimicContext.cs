123bf2fc3a41c36077a7b43c8ab3fe6bae09babb
amandamata <amanda.mata@live.com>
2020-09-19 00:16:30 -0300

    entidades e configuracao database

---
+++ MimicAPI/Database/MimicContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MimicAPI.Model;
+
+
+namespace MimicAPI.Database
+{
+    public class MimicContext : DbContext
+    {
+        public MimicContext(DbContextOptions<MimicContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Palavra> Palavras { get; set; }
+
+    }
+}
--- MimicAPI/Startup.cs
+++ MimicAPI/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -16,6 +18,11 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MimicContext>(opt =>
+            {
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +32,8 @@ namespace MimicAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+                
+            app.UseMvc();
         }
     }
 }
