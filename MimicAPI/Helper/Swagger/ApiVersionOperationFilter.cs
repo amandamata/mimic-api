e8dd2424aade58fc0e4980302f4743aa80523f22
amandamata <amanda.mata@live.com>
2020-09-25 09:14:21 -0300

    versionament swagger

---
+++ MimicAPI/Helper/Swagger/ApiVersionOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;  
+using  Microsoft.OpenApi.Models;  
+using  Swashbuckle.AspNetCore.SwaggerGen;  
+using  Swashbuckle.AspNetCore.Swagger;  
+using  System.Collections.Generic;
+using System.Linq;
+
+namespace MimicAPI.Helper.Swagger
+{
+    public class ApiVersionOperationFilter
+    {
+        public void Apply(Microsoft.AspNetCore.JsonPatch.Operations.Operation operation, OperationFilterContext context)
+        {
+            var actionApiVersionModel = context.ApiDescription.ActionDescriptor?.GetApiVersion();
+            if (actionApiVersionModel == null)
+            {
+                return;
+            }
+
+            if (actionApiVersionModel.DeclaredApiVersions.Any())
+            {
+                operation.op.SelectMany(p => actionApiVersionModel.DeclaredApiVersions
+                    .Select(version => $"{p};v={version.ToString()}")).ToList();
+            }
+            else
+            {
+                operation.op.SelectMany(p => actionApiVersionModel.ImplementedApiVersions.OrderByDescending(v => v)
+                    .Select(version => $"{p};v={version.ToString()}")).ToList();
+            }
+        }
+    }
+}
--- MimicAPI/Startup.cs
+++ MimicAPI/Startup.cs
@@ -10,13 +10,12 @@ using AutoMapper;
 using MimicAPI.Helper;
 using System.Linq;
 using System;
+using MimicAPI.Helper.Swagger;
 
 namespace MimicAPI
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             #region AutoMapper-Config
@@ -41,11 +40,22 @@ namespace MimicAPI
             services.AddSwaggerGen(c =>
             {
                 c.ResolveConflictingActions(apiDescription => apiDescription.First());
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "MimicAPI - V1", Version = "V1" });
+                c.SwaggerDoc("v2.0", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "MimicAPI - v2.0", Version = "v2.0" });
+                c.SwaggerDoc("v1.1", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "MimicAPI - v1.1", Version = "v1.1" });
+                c.SwaggerDoc("v1.0", new Microsoft.OpenApi.Models.OpenApiInfo() { Title = "MimicAPI - v1.0", Version = "v1.0" });
+                
+                c.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    var actionApiVersionModel = apiDesc.ActionDescriptor?.GetApiVersion();
+                    if (actionApiVersionModel == null) return true;
+                    if (actionApiVersionModel.DeclaredApiVersions.Any())
+                        return actionApiVersionModel.DeclaredApiVersions.Any(v => $"v{v.ToString()}" == docName);
+                    return actionApiVersionModel.ImplementedApiVersions.Any(v => $"v{v.ToString()}" == docName);
+
+                });
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
@@ -53,7 +63,9 @@ namespace MimicAPI
             app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json","MimicAPI");
+                c.SwaggerEndpoint("/swagger/v2.0/swagger.json", "MimicAPI - v2.0");
+                c.SwaggerEndpoint("/swagger/v1.1/swagger.json", "MimicAPI - v1.1");
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "MimicAPI - v1.0");
                 c.RoutePrefix = String.Empty;
             });
         }
